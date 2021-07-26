bcedc798e4da038e1132b2e0ae148a363643eaf5
Maria Kowalska <maria.kowalska@stxnext.pl>
2021-07-26 13:20:54 +0200

    adding postgres and django-environ

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM python:3
 
 ENV PYTHONUNBUFFERED=1
 
-ENV VIRTUAL_ENV=/venv
-RUN python3 -m venv $VIRTUAL_ENV
-ENV PATH="$VIRTUAL_ENV/bin:$PATH"
-
 WORKDIR /code
 COPY requirements.txt /code/
+
+RUN /usr/local/bin/python -m pip install --upgrade pip
+# RUN useradd -u 101 python
+# USER python
 RUN pip install -r requirements.txt
 COPY . /code/
--- trello/settings.py
+++ trello/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
+
+env = environ.Env(
+            DEBUG=(bool, False)
+        )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r(i@i*pd)iw2cni*$v4yzm&1k#4n_vb32it)znzr5o_k-i+_@s'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,10 +80,7 @@ WSGI_APPLICATION = 'trello.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
