2ea96d659a98429ecc2cab14dafba95167a1fb32
Maria Kowalska <maria.kowalska@stxnext.pl>
2021-07-29 09:42:17 +0200

    hide password in serializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,10 +3,13 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(style={'input_type': 'password'},
+                                     write_only=True,
+                                     min_length=8)
+
     class Meta:
         model = get_user_model()
         fields = ['email', 'password']
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
