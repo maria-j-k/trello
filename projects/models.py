d247107804dc11ab4d16b19c86adcd6997754b40
Maria Kowalska <maria.kowalska@stxnext.pl>
2021-08-10 16:49:46 +0200

    created_date added to serializer

--- projects/models.py
+++ projects/models.py
@@ -16,37 +16,3 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# class Issue(models.Model):
-#    TODO = 1
-#    IN_PROGRESS = 2
-#    REVIEW = 3
-#    DONE = 4
-#    STATUS_CHOICES = [
-#        (TODO, 'todo'),
-#        (IN_PROGRESS, 'in progress'),
-#        (REVIEW, 'review'),
-#        (DONE, 'done'),
-#    ]
-#    title = models.CharField(max_length=100)
-#    description = models.TextField()
-#    created_date = models.DateTimeField(auto_now_add=True)
-#    due_date = models.DateField()
-#    project = models.ForeignKey(Project,
-#                                related_name='issues',
-#                                on_delete=models.CASCADE,
-#                                blank=True, null=True)
-#    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                              related_name='owned',
-#                              on_delete=models.CASCADE,
-#                              blank=True, null=True)
-#    assignee = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                                 related_name='assigned',
-#                                 on_delete=models.CASCADE,
-#                                 blank=True, null=True)
-#    status = models.IntegerField(choices=STATUS_CHOICES, null=True)
-# Issue should consist of:
-#   "title", "description", "created date", "due date", "owner",
-#   "assignee", "status".
-# Possible statuses: "todo", "in progress", "review", "done"
--- projects/serializers.py
+++ projects/serializers.py
@@ -8,8 +8,8 @@ class ProjectCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Project
-        fields = ['id', 'name', 'owner']
-        read_only_fields = ['id', 'owner']
+        fields = ['id', 'name', 'owner', 'created_date']
+        read_only_fields = ['id', 'owner', 'created_date']
 
 
 class ProjectAssignSerializer(serializers.ModelSerializer):
